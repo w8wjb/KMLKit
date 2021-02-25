5dd2d2ca42830bba4669951a2de6fa2702b6512e
Weston Bustraan <wbustraan@gmail.com>
2021-02-25 09:59:24 -0500

    Initial Commit

---
+++ KMLKit/KMLKit.h
@@ -0,0 +1,18 @@
+//
+//  KMLKit.h
+//  KMLKit
+//
+//  Created by Weston Bustraan on 2/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KMLKit.
+FOUNDATION_EXPORT double KMLKitVersionNumber;
+
+//! Project version string for KMLKit.
+FOUNDATION_EXPORT const unsigned char KMLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMLKit/PublicHeader.h>
+
+
