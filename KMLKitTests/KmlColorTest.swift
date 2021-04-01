42c718b870cb60b79febaf7e3f4375fc4a843849
Weston Bustraan <wbustraan@gmail.com>
2021-03-31 21:36:18 -0400

    - Fixed color tests to follow color change

--- KMLKitTests/KmlColorTest.swift
+++ KMLKitTests/KmlColorTest.swift
@@ -12,17 +12,17 @@ class KmlColorTest: XCTestCase {
 
     func testColorHexRoundTrip() throws {
         
-        XCTAssertEqual("ffffffff", KMLColor.white.usingColorSpace(.sRGB)!.hexRGBaColor)
-        XCTAssertEqual("000000ff", KMLColor.black.usingColorSpace(.sRGB)!.hexRGBaColor)
+        XCTAssertEqual("ffffffff", KMLColor.white.usingColorSpace(.sRGB)!.kmlHex)
+        XCTAssertEqual("ff000000", KMLColor.black.usingColorSpace(.sRGB)!.kmlHex)
         
-        let color = KMLColor(hex: "12345678")
+        let color = KMLColor(kmlHex: "78563412")
         XCTAssertEqual(18 / 255, color.redComponent, accuracy: 0.0001)
         XCTAssertEqual(52 / 255, color.greenComponent)
         XCTAssertEqual(86 / 255, color.blueComponent)
         XCTAssertEqual(120 / 255, color.alphaComponent)
-        XCTAssertEqual("12345678", color.hexRGBaColor)
+        XCTAssertEqual("78563412", color.kmlHex)
         
-        XCTAssertEqual("ffa8ff00", KMLColor(hex: "ffa8ff00").hexRGBaColor)
+        XCTAssertEqual("ffa8ff00", KMLColor(kmlHex: "ffa8ff00").kmlHex)
     }
 
 }
